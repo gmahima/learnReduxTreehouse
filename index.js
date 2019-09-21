88d3ef7d58d06fec3e77ba2ce963a0dea2811c4b
Mahima Gangavarapu <39587007+gmahima@users.noreply.github.com>
2019-09-21 19:07:54 +0530

    fix file linking bugs

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import React from "react";
 import { render } from "react-dom";
 import Scoreboard from "./Scoreboard";
+import "./app.css";
 render(<Scoreboard />, document.getElementById("root"));
