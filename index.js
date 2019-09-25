7380a58d0708dc764b51784ac974dc6be572338b
Mahima Gangavarapu <39587007+gmahima@users.noreply.github.com>
2019-09-25 20:05:21 +0530

    add Stopwatch.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import { render } from "react-dom";
-import Scoreboard from "./Scoreboard";
-import "./app.css";
+import Scoreboard from "./src/containers/Scoreboard";
+//import "./app.css";
 render(<Scoreboard />, document.getElementById("root"));
