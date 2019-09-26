bdfa0f12325e0eeb13b84879123d3fd87323fe35
Mahima Gangavarapu <39587007+gmahima@users.noreply.github.com>
2019-09-26 20:30:06 +0530

    add scoreboard

--- src/containers/Scoreboard.js
+++ src/containers/Scoreboard.js
@@ -25,27 +25,20 @@ class Scoreboard extends Component {
       dispatch
     );
     const playerComponents = players.map((player, index) => (
-      <Player index={index} name={player.name} score={player.score} key = {player.name}/>
+      <Player
+        index={index}
+        name={player.name}
+        score={player.score}
+        key={player.name}
+        updatePlayerScore={updatePlayerScore}
+        removePlayer={removePlayer}
+      />
     ));
     return (
       <div className="scoreboard">
-        <Header players={this.state.players} />
-        <div className="players">
-          {this.state.players.map(
-            function(player, index) {
-              return (
-                <Player
-                  name={player.name}
-                  score={player.score}
-                  key={player.name}
-                  onScoreChange={delta => this.onScoreChange(index, delta)}
-                  onRemove={() => this.onRemovePlayer(index)}
-                />
-              );
-            }.bind(this)
-          )}
-        </div>
-        <AddPlayerForm onAdd={this.onAddPlayer} />
+        <Header players={players} />
+        <div className="players">{playerComponents}</div>
+        <AddPlayerForm addPlayer={addPlayer} />
       </div>
     );
   }
